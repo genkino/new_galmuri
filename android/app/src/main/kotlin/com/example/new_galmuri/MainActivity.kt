94863451cdcaa78428dafd7ae830f044ec02668b
genkino <genkino@gmail.com>
2025-05-03 15:07:06 +0900

    fixing android issues

--- android/app/src/main/kotlin/com/example/new_galmuri/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.new_galmuri
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/genkino/new_galmuri/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.genkino.new_galmuri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
