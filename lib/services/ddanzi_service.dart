091c3da1dada1f0d1aa46a0e14da4b4d8e3f3339
genkino <genkino@gmail.com>
2025-05-04 10:02:34 +0900

    딴지 시간 계산 수정

--- lib/services/ddanzi_service.dart
+++ lib/services/ddanzi_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
+import 'package:html/parser.dart' show parse;
+import 'package:intl/intl.dart';
 import '../models/post.dart';
 import 'base_service.dart';
 
@@ -64,7 +65,7 @@ class DdanziService extends BaseBoardService {
     final views = int.tryParse(viewsElement.text.trim().replaceAll(',', '')) ?? 0;
     
     final timeStr = timeElement.text.trim();
-    final timestamp = parseTimestamp(timeStr);
+    final timestamp = _parseTimestamp(timeStr);
     final url = buildFullUrl(linkElement.attributes['href'] ?? '');
     
     return Post(
@@ -74,5 +75,45 @@ class DdanziService extends BaseBoardService {
       timestamp: timestamp,
       url: url,
     );
+  }
+
+  DateTime _parseTimestamp(String timestamp) {
+    try {
+      if (timestamp.contains('분 전')) {
+        final minutes = int.parse(timestamp.replaceAll('분 전', ''));
+        return DateTime.now().subtract(Duration(minutes: minutes));
+      } else if (timestamp.contains('시간 전')) {
+        final hours = int.parse(timestamp.replaceAll('시간 전', ''));
+        return DateTime.now().subtract(Duration(hours: hours));
+      } else if (timestamp.contains('일 전')) {
+        final days = int.parse(timestamp.replaceAll('일 전', ''));
+        return DateTime.now().subtract(Duration(days: days));
+      } else {
+        // 시간만 있는 경우 (HH:mm)
+        if (timestamp.contains(':') && !timestamp.contains('.')) {
+          final now = DateTime.now();
+          final timeParts = timestamp.split(':');
+          return DateTime(now.year, now.month, now.day, 
+            int.parse(timeParts[0]), int.parse(timeParts[1]));
+        }
+        // 날짜만 있는 경우 (YYYY.MM.DD)
+        else if (timestamp.contains('.')) {
+          final dateParts = timestamp.split('.');
+          return DateTime(
+            int.parse(dateParts[0]), // year
+            int.parse(dateParts[1]), // month
+            int.parse(dateParts[2]), // day
+            23, 59, 0 // 23:59:00
+          );
+        }
+        // 날짜와 시간이 모두 있는 경우 (YYYY.MM.DD HH:mm)
+        else {
+          return DateFormat('yyyy.MM.dd HH:mm').parse(timestamp);
+        }
+      }
+    } catch (e) {
+      print('Error parsing timestamp: $e');
+      return DateTime.now();
+    }
   }
 } 
